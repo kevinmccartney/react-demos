46a4b4a43a74e2cfa0121fa741c9e592cdc63c4c
Kevin McCartney <hello@kevinmccartney.is>
2016-12-05 19:46:14 -0500

    Added redux-devtools

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux'
 import App from './containers/App'
 import reducer from './reducers'
 
-const store = createStore(reducer)
+const store = createStore(reducer, composeWithDevTools())
 
 render(
   <Provider store={store}>
