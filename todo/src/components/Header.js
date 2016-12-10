2056dab6abd95cdb8bb8c008b5e8ff90f233d204
Kevin McCartney <hello@kevinmccartney.is>
2016-12-09 23:55:07 -0500

    Adding styles.

--- todo/src/components/Header.js
+++ todo/src/components/Header.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react'
 export default class Header extends Component {
   render() {
     return(
-      <nav className="navbar navbar-default">
+      <nav className="todo-navbar navbar navbar-default">
         <div className="container-fluid">
           <div className="navbar-header">
-            <a className="navbar-brand" href="#">
-              <img alt="TodoApp" src="" />
+            <a className="todo-logo navbar-brand" href="#">
+              <h3>Todo App</h3>
             </a>
           </div>
         </div>
