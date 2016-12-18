39391ad8efb8664723c6f687c042fe896b52995a
Kevin McCartney <hello@kevinmccartney.is>
2016-12-18 12:30:41 -0500

    Finished testing todo actions

--- todo/src/actions/index.spec.js
+++ todo/src/actions/index.spec.js
@@ -2,12 +2,35 @@
 // likely separate the actions into their own files
 
 import * as actions from './index'
+import * as types from '../constants/Todos'
 
 describe('todo actions', () =>{
   it('addTodo should create ADD_TODO action', () => {
     expect(actions.addTodo('Test your actions!')).toEqual({
-      type: 'ADD_TODO',
+      type: types.ADD_TODO,
       text: 'Test your actions!'
+    })
+  })
+
+  it('deleteTodo should create DELETE_TODO action', () => {
+    expect(actions.deleteTodo(1)).toEqual({
+      type: types.DELETE_TODO,
+      id: 1
+    })
+  })
+
+  it('editTodo should create a EDIT_TODO action', () => {
+    expect(actions.editTodo(1, "New todo text")).toEqual({
+      type: types.EDIT_TODO,
+      id: 1,
+      text: "New todo text"
+    })
+  })
+
+  it("completeTodo should create a COMPLETE_TODO action", () => {
+    expect(actions.completeTodo(1)).toEqual({
+      type: types.COMPLETE_TODO,
+      id: 1
     })
   })
 
